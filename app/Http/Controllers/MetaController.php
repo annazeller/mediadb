a842fc6f769b8a99f2813163fed40b30dab3e849
unknown <Kiim125>
2019-02-11 16:54:49 +0100

    iptc vollständig filter teilweise

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 use App\Iptc;
 use Image;
+use App\Category;
 
 class MetaController extends Controller
 {
@@ -18,43 +19,43 @@ class MetaController extends Controller
     public function getimage(Request $request){
         $imagesource = $request['imagesource'];
         $path = storage_path($imagesource);
-        $iptc = Image::make($path)->iptc();
-        $iptc = serialize($iptc);
-        return redirect('/iptc')->with("status", $imagesource)->with("iptc", $iptc);
+        $documenttitle = Image::make($path)->iptc(' DocumentTitle');
+        $urgency = Image::make($path)->iptc('Urgency');
+        $category = Image::make($path)->iptc('Category');
+        $subcategories = Image::make($path)->iptc('Subcategories');
+        $keywords = Image::make($path)->iptc('Keywords');
+        $specialinstructions = Image::make($path)->iptc('SpecialInstructions');
+        $autor = Image::make($path)->iptc('AuthorByline');
+        $city = Image::make($path)->iptc('City');
+        $state = Image::make($path)->iptc('State');
+        $country = Image::make($path)->iptc('Country');
+        $otr = Image::make($path)->iptc('OTR');
+        $photosource= Image::make($path)->iptc('PhotoSource');
+        $copyright = Image::make($path)->iptc('Copyright');
+        $caption = Image::make($path)->iptc('Caption');
+        $creationdate = Image::make($path)->iptc('CreationDate');
+        $creationtime = Image::make($path)->iptc('CreationTime');
+        return redirect('/iptc')->with("status", $imagesource)->with("documenttitle", $documenttitle)->with("urgency", $urgency)->with("category", $category)->with("subcategories", $subcategories)->with("keywords", $keywords)->with("specialinstructions", $specialinstructions)->with("autor", $autor)->with("city", $city)->with("state", $state)->with("country", $country)->with("otr", $otr)->with("photosource", $photosource)->with("copyright", $copyright)->with("caption", $caption)->with("creationdate", $creationdate)->with("creationtime", $creationtime);
     }
 
     public function iptc(Request $request){
 
         $object_name = $request->input('object_name');
-        $edit_status = $request->input('edit_status');
         $priority = $request->input('priority');
         $category = $request->input('category');
         $supplemental_category = $request->input('supplemental_category');
-        $fixture_identifer = $request->input('fixture_identifer');
         $keywords = $request->input('keywords');
-        $release_date = $request->input('release_date');
-        $release_time = $request->input('release_time');
         $special_instructions = $request->input('special_instructions');
-        $reference_service = $request->input('reference_service');
-        $reference_date = $request->input('reference_date');
-        $reference_number = $request->input('reference_number');
         $created_date = $request->input('created_date');
         $created_time = $request->input('created_time');
-        $originating_program = $request->input('originating_program');
-        $program_version = $request->input('program_version');
-        $object_cycle = $request->input('object_cycle');
         $creator = $request->input('creator');
         $city= $request->input('city');
         $province_state= $request->input('province_state');
-        $country_code= $request->input('country_code');
         $country= $request->input('country');
         $original_transmission_reference = $request->input('original_transmission_reference');
-        $headline = $request->input('headline');
-        $credit = $request->input('credit');
         $source = $request->input('source');
         $copyright_string = $request->input('copyright_string');
         $caption = $request->input('caption');
-        $local_caption = $request->input('local_caption');
         $caption_writer = $request->input('caption_writer');
 
         $imagesource = $request->input('imagesource');
@@ -62,35 +63,30 @@ class MetaController extends Controller
 
         $iptc = new Iptc($contents);
         if(!empty($object_name)) { $iptc->set(Iptc::OBJECT_NAME, array($object_name)); }
-        if(!empty($edit_status)) { $iptc->set(Iptc::EDIT_STATUS, array($edit_status)); }
         if(!empty($priority)) { $iptc->set(Iptc::PRIORITY , array($priority)); }
-        if(!empty($category)) { $iptc->set(Iptc::CATEGORY, array($category)); }
+        if(!empty($category)) {
+            $iptc->set(Iptc::CATEGORY, array($category));
+            if (Category::where('name', '=',$category)->exists()) {}
+            else {
+                Category::create([
+                    'name' => $category,
+                    'user_id' => Auth::id(),
+                ]);
+            }
+        }
         if(!empty($supplemental_category)) { $iptc->set(Iptc::SUPPLEMENTAL_CATEGORY, array($supplemental_category)); }
-        if(!empty($fixture_identifer)) { $iptc->set(Iptc::FIXTURE_IDENTIFIER, array($fixture_identifer)); }
         if(!empty($keywords)) { $iptc->set(Iptc::KEYWORDS, array($keywords)); }
-        if(!empty($release_date)) { $iptc->set(Iptc::RELEASE_DATE, array($release_date)); }
-        if(!empty($release_time)) { $iptc->set(Iptc::RELEASE_TIME, array($release_time)); }
         if(!empty($special_instructions)) { $iptc->set(Iptc::SPECIAL_INSTRUCTIONS , array($special_instructions)); }
-        if(!empty($reference_service)) { $iptc->set(Iptc::REFERENCE_SERVICE, array($reference_service)); }
-        if(!empty($reference_date)) { $iptc->set(Iptc::REFERENCE_DATE, array($reference_date)); }
-        if(!empty($reference_number)) { $iptc->set(Iptc::REFERENCE_NUMBER, array($reference_number)); }
         if(!empty($created_date)) { $iptc->set(Iptc::CREATED_DATE , array($created_date)); }
         if(!empty($created_time)) { $iptc->set(Iptc::CREATED_TIME, array($created_time)); }
-        if(!empty($originating_program)) { $iptc->set(Iptc::ORIGINATING_PROGRAM, array($originating_program)); }
-        if(!empty($program_version)) { $iptc->set(Iptc::PROGRAM_VERSION, array($program_version)); }
-        if(!empty($object_cycle)) { $iptc->set(Iptc::OBJECT_CYCLE , array($object_cycle)); }
         if(!empty($creator)) { $iptc->set(Iptc::CREATOR , array($creator)); }
         if(!empty($city)) { $iptc->set(Iptc::CITY, array($city)); }
         if(!empty($province_state)) { $iptc->set(Iptc::PROVINCE_STATE, array($province_state)); }
-        if(!empty($country_code)) { $iptc->set(Iptc::COUNTRY_CODE, array($country_code)); }
         if(!empty($country)) { $iptc->set(Iptc::COUNTRY , array($country)); }
         if(!empty($original_transmission_reference)) { $iptc->set(Iptc::ORIGINAL_TRANSMISSION_REFERENCE, array($original_transmission_reference)); }
-        if(!empty($headline)) { $iptc->set(Iptc::HEADLINE, array($headline)); }
-        if(!empty($credit)) { $iptc->set(Iptc::CREDIT , array($credit)); }
         if(!empty($source)) { $iptc->set(Iptc::SOURCE, array($source)); }
         if(!empty($copyright_string)) { $iptc->set(Iptc::COPYRIGHT_STRING, array($copyright_string)); }
         if(!empty($caption)) { $iptc->set(Iptc::CAPTION, array($caption)); }
-        if(!empty($local_caption)) { $iptc->set(Iptc::LOCAL_CAPTION, array($local_caption)); }
         if(!empty($caption_writer)) { $iptc->set(Iptc::CAPTION_WRITER, array($caption_writer)); }
         $iptc->write();
         return redirect('/');
--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,14 +1,22 @@
-@if (session("iptc"))
-    <div class="alert alert-success">
-        {{ session("iptc") }}
-    </div>
-@endif
 
-@if (session("status"))
-    <div class="d-none alert alert-success">
-        {{ session("status") }}
-    </div>
-@endif
+Objektname: {{ session("documenttitle") }} <br>
+Priorität: {{ session("urgency") }}<br>
+Kategorie: {{ session("category") }} <br>
+Zusätzliche Kategorie: {{ session("subcategories") }} <br>
+Keywords: {{ session("keywords") }} <br>
+Spezielle Anleitung: {{ session("specialinstructions") }} <br>
+Autor: {{ session("autor") }} <br>
+Stadt: {{ session("city") }} <br>
+Provinz-Staat: {{ session("state") }} <br>
+Land: {{ session("country") }} <br>
+Ursprüngliche Übertragungsreferenz: {{ session("otr") }} <br>
+Quelle: {{ session("photosource") }} <br>
+Copyright: {{ session("copyright") }} <br>
+Bildunterschrift: {{ session("caption") }} <br>
+Erstellungs-Datum: {{ session("creationdate") }} <br>
+Erstellungs-Zeit: {{ session("creationtime") }} <br> <br>
+
+
 <form class="form-horizontal" method="post" action="/post">
     <input name="imagesource" type="hidden" value="{{ session("status") }}">
     <div class="form-group">
@@ -17,14 +25,6 @@
         </label>
         <div class="col-lg-10">
             <input type="text" class="form-control" name="object_name">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="data" class="col-lg-2 control-label">
-            Bearbeitungs-Status
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="edit_status">
         </div>
     </div>
     <div class="form-group">
@@ -45,18 +45,10 @@
     </div>
     <div class="form-group">
         <label for="value" class="col-lg-2 control-label">
-            Zusätzliche Kategorie
+            Unterkategorie
         </label>
         <div class="col-lg-10">
             <input type="text" class="form-control" name="supplemental_category">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Vorrichtung identifizieren
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="fixture_identifier">
         </div>
     </div>
     <div class="form-group">
@@ -69,50 +61,10 @@
     </div>
     <div class="form-group">
         <label for="value" class="col-lg-2 control-label">
-            Veröffentlichkeits-Datum
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="release_date">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Veröffentlichkeits-Zeit
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="release_time">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
             Spezielle Anleitung
         </label>
         <div class="col-lg-10">
             <input type="text" class="form-control" name="special_instructions">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Referenz Service
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="reference_service">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Referenz Datum
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="reference_date">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Referenz Nummer
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="reference_number">
         </div>
     </div>
     <div class="form-group">
@@ -129,30 +81,6 @@
         </label>
         <div class="col-lg-10">
             <input type="text" class="form-control" name="created_time">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Ursprungs-Programm
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="originating_program">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Programm-Version
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="program_version">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Objekt-Zyklus
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="object_cycle">
         </div>
     </div>
     <div class="form-group">
@@ -181,14 +109,6 @@
     </div>
     <div class="form-group">
         <label for="value" class="col-lg-2 control-label">
-            Länder-Code
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="country_code">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
             Land
         </label>
         <div class="col-lg-10">
@@ -201,22 +121,6 @@
         </label>
         <div class="col-lg-10">
             <input type="text" class="form-control" name="original_transmission_reference">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Überschrift
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="headline">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Credit
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="credit">
         </div>
     </div>
     <div class="form-group">
@@ -241,14 +145,6 @@
         </label>
         <div class="col-lg-10">
             <input type="text" class="form-control" name="caption">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="value" class="col-lg-2 control-label">
-            Lokale Bildunterschrift
-        </label>
-        <div class="col-lg-10">
-            <input type="text" class="form-control" name="local_caption">
         </div>
     </div>
     <div class="form-group">
